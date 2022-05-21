13e1c250ce88b26b318e7ff4382f66c77fecbbe0
vijay-garg <vijay24garg@gmail.com>
2022-05-21 17:03:10 +0530

    use VS code

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,32 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import InputField from "./components/InputField";
-import {Todo} from "./model";
+import { Todo } from "./model";
 import TodoList from "./components/TodoList";
 
-
 const App: React.FC = () => {
-    /*
+  /*
      Returns a stateful value, and a function to update it.
      Input is the initial state
      */
-    const [todo, setTodo] = useState<string>("");
-    const [todos, setTodos] = useState<Todo[]>([]);
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-    const handleAdd = (e: React.FormEvent) => {
-        e.preventDefault();
-        if (todo) {
-            setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}]);
-        }
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
     }
+  };
 
-    console.log(todos);
-    return (
-        <div className="App">
-            <span className="heading">Taskify</span>
-            <InputField todo={todo}
-                        setTodo={setTodo}
-                        handleAdd={handleAdd}/>
-            <TodoList todos={todos}
-                      setTodos={setTodos}/>
-        </div>
-    );
-}
+  console.log(todos);
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
+  );
+};
 
 export default App;
