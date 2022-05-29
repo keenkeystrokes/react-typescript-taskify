d1b57cdcf21a18ffede64d52de65cc68994b3986
vijay-garg <vijay24garg@gmail.com>
2022-05-29 13:28:24 +0530

    add functionalities

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "./styles.css";
 import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
 
 interface Props {
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
+const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
   return (
     <div className="todos">
       {todos.map((todo) => (
-        <li>{todo.todo}</li>
+        <SingleTodo todo={todo} todos={todos} setTodos={setTodos} />
       ))}
     </div>
   );
